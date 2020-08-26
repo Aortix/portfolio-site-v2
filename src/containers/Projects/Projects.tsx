1f4d86417fe099a31a3c1dea297bdb54cde71adf
Stephen Riding <ridingjr@gmail.com>
2020-08-26 10:06:51 -0700

    .

--- src/containers/Projects/Projects.tsx
+++ src/containers/Projects/Projects.tsx
@@ -1,23 +1,23 @@
-import React, { Suspense, Fragment } from "react";
-import styles from "./Projects.module.css";
-import { Transition } from "react-transition-group";
+import React, { Suspense, Fragment } from 'react';
+import styles from './Projects.module.css';
+import { Transition } from 'react-transition-group';
 
 //Images
-import BuddyConnectImage from "./../../images/buddyconnect.png";
-import DodgeTheObjectsImage from "./../../images/dodge-the-objects-screenshot.jpg";
-import JobKeeperLogImage from "./../../images/job-seeker-log.png";
-import MoviesForEveryoneImage from "./../../images/moviesforeveryone-mainpage.png";
-import BudgetAppImage from "./../../images/budgetelectronapp1.png";
-import TaskAppImage from "./../../images/weeklytasks-iphone.png";
-import PostShiftImage from "./../../images/post-shift.png";
-import ShortStoriesImage from "./../../images/shortstories.png";
-import AppStoreLogo from "./../../images/appstorelogo.svg";
+import BuddyConnectImage from './../../images/buddyconnect.png';
+import DodgeTheObjectsImage from './../../images/dodge-the-objects-screenshot.jpg';
+import JobKeeperLogImage from './../../images/job-seeker-log.png';
+import MoviesForEveryoneImage from './../../images/moviesforeveryone-mainpage.png';
+import BudgetAppImage from './../../images/budgetelectronapp1.png';
+import TaskAppImage from './../../images/weeklytasks-iphone.png';
+import PostShiftImage from './../../images/post-shift.png';
+import ShortStoriesImage from './../../images/shortstories.png';
+import AppStoreLogo from './../../images/appstorelogo.svg';
 
 //Transitions
-import { easeInOut } from "./../../transitions/easeInOut";
+import { easeInOut } from './../../transitions/easeInOut';
 
 //Components
-const Project = React.lazy(() => import("./../../components/Project/Project"));
+const Project = React.lazy(() => import('./../../components/Project/Project'));
 
 interface AppProps {
   isVisible: string;
@@ -26,7 +26,7 @@ interface AppProps {
 }
 
 const Projects = React.memo((props: AppProps) => {
-  if (props.isVisible === "PROJECTS") {
+  if (props.isVisible === 'PROJECTS') {
     return (
       <Transition in={props.inProp} timeout={easeInOut.duration}>
         {(state) => (
@@ -59,44 +59,44 @@ const Projects = React.memo((props: AppProps) => {
               <Suspense fallback={<Fragment />}>
                 <Project
                   darkTheme={props.darkTheme}
-                  projectName="Shortstories"
-                  projectGitHubLink="https://github.com/Aortix/Shortstories"
-                  projectLink="https://www.short-storys.com/"
-                  projectImage={ShortStoriesImage}
-                  projectTechnologies={[
-                    "C#",
-                    "MVC",
-                    ".NET Core",
-                    "EF Core",
-                    "SQL Server",
-                    "Firebase Auth",
-                    "HTML",
-                    "CSS",
-                    "Bootstrap",
-                    "JavaScript",
-                  ]}
-                  projectDescription="A .NET Core web application that allows user registration and users can write out their own short stories and they will be displayed on the site. Users can follow other users so that their stories
-                  appear in their follower feed and there is a filter feed that will display any and all stories based off the selected filter."
-                />
-              </Suspense>
-              <Suspense fallback={<Fragment />}>
-                <Project
-                  darkTheme={props.darkTheme}
                   projectName="Job Seeker Log"
                   projectGitHubLink="https://github.com/Aortix/JobSeekerLog"
                   projectLink="https://jobseekerlog.herokuapp.com/"
                   projectImage={JobKeeperLogImage}
                   projectTechnologies={[
-                    "PHP",
-                    "MySQL",
-                    "Linux",
-                    "Apache",
-                    "HTML",
-                    "CSS",
-                    "Bootstrap",
-                    "JavaScript",
+                    'PHP',
+                    'MySQL',
+                    'Linux',
+                    'Apache',
+                    'HTML',
+                    'CSS',
+                    'Bootstrap',
+                    'JavaScript',
                   ]}
                   projectDescription="LAMP web application for saving information about job applications while searching for a job. Job information will be saved to the user's profile and can be searched through."
+                />
+              </Suspense>
+              <Suspense fallback={<Fragment />}>
+                <Project
+                  darkTheme={props.darkTheme}
+                  projectName="Shortstories"
+                  projectGitHubLink="https://github.com/Aortix/Shortstories"
+                  projectLink="https://shortstories-app.herokuapp.com/"
+                  projectImage={ShortStoriesImage}
+                  projectTechnologies={[
+                    'C#',
+                    'MVC',
+                    '.NET Core',
+                    'EF Core',
+                    'SQL Server',
+                    'Firebase Auth',
+                    'HTML',
+                    'CSS',
+                    'Bootstrap',
+                    'JavaScript',
+                  ]}
+                  projectDescription="A .NET Core web application that allows user registration and users can write out their own short stories and they will be displayed on the site. Users can follow other users so that their stories
+                  appear in their follower feed and there is a filter feed that will display any and all stories based off the selected filter."
                 />
               </Suspense>
               <Suspense fallback={<Fragment />}>
@@ -107,12 +107,12 @@ const Projects = React.memo((props: AppProps) => {
                   projectLink="https://sriding-blog.com/"
                   projectImage={PostShiftImage}
                   projectTechnologies={[
-                    "PHP",
-                    "WordPress",
-                    "Theme Development",
-                    "HTML",
-                    "CSS",
-                    "JavaScript",
+                    'PHP',
+                    'WordPress',
+                    'Theme Development',
+                    'HTML',
+                    'CSS',
+                    'JavaScript',
                   ]}
                   projectDescription="post-shift is a custom WordPress theme that I created. The site displayed above is a site of mine that is using the theme."
                 />
@@ -125,16 +125,16 @@ const Projects = React.memo((props: AppProps) => {
                   projectLink="https://buddyconnecter.herokuapp.com/"
                   projectImage={BuddyConnectImage}
                   projectTechnologies={[
-                    "HTML",
-                    "CSS",
-                    "JavaScript",
-                    "React",
-                    "Redux",
-                    "Node/Express",
-                    "Passport.js",
-                    "JWTs",
-                    "MongoDB",
-                    "AWS S3",
+                    'HTML',
+                    'CSS',
+                    'JavaScript',
+                    'React',
+                    'Redux',
+                    'Node/Express',
+                    'Passport.js',
+                    'JWTs',
+                    'MongoDB',
+                    'AWS S3',
                   ]}
                   projectDescription="MERN web application that features user, profile, avatar, post, and comment creation using a REST API and authentication with Passport.js and JWTs."
                 />
@@ -147,10 +147,10 @@ const Projects = React.memo((props: AppProps) => {
                   projectLink="https://dodgetheobjects.com"
                   projectImage={DodgeTheObjectsImage}
                   projectTechnologies={[
-                    "HTML",
-                    "CSS",
-                    "JavaScript",
-                    "Canvas API",
+                    'HTML',
+                    'CSS',
+                    'JavaScript',
+                    'Canvas API',
                   ]}
                   projectDescription="A Canvas game where you dodge the objects that are flying at you until you reach the end of the timer. Features object collision, movement, and invincibility."
                 />
@@ -163,12 +163,12 @@ const Projects = React.memo((props: AppProps) => {
                   projectLink="https://moviesforeveryone.herokuapp.com/"
                   projectImage={MoviesForEveryoneImage}
                   projectTechnologies={[
-                    "HTML",
-                    "CSS",
-                    "JavaScript",
-                    "React",
-                    "Redux",
-                    "Node/Express",
+                    'HTML',
+                    'CSS',
+                    'JavaScript',
+                    'React',
+                    'Redux',
+                    'Node/Express',
                   ]}
                   projectDescription="React and Node web application for searching for potential movies to watch. Features filtering, pagination, and making requests to an external 3rd party API."
                 />
@@ -196,23 +196,23 @@ const Projects = React.memo((props: AppProps) => {
                 projectLink="https://apps.apple.com/us/app/weekly-task-planner/id1475793415?ls=1"
                 projectMobileImage={TaskAppImage}
                 projectTechnologies={[
-                  "HTML",
-                  "CSS",
-                  "TypeScript",
-                  "React Native",
-                  "React Native Paper",
-                  "Realm",
+                  'HTML',
+                  'CSS',
+                  'TypeScript',
+                  'React Native',
+                  'React Native Paper',
+                  'Realm',
                 ]}
                 projectDescription="A weekly tasks mobile application that keeps track of your daily tasks that you do throughout the week and resets them every monday. Features task creation and storage using an embedded database system, local notifications, and material design."
                 projectPlayStore={{
                   target:
-                    "https://play.google.com/store/apps/details?id=com.weeklytasks&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1",
+                    'https://play.google.com/store/apps/details?id=com.weeklytasks&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1',
                   source:
-                    "https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png",
+                    'https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png',
                 }}
                 projectAppStore={{
                   target:
-                    "https://apps.apple.com/us/app/weekly-task-planner/id1475793415?ls=1",
+                    'https://apps.apple.com/us/app/weekly-task-planner/id1475793415?ls=1',
                   source: AppStoreLogo,
                 }}
               />
@@ -234,13 +234,13 @@ const Projects = React.memo((props: AppProps) => {
                 projectLink="https://github.com/Aortix/Budget-of-the-Week/releases"
                 projectImage={BudgetAppImage}
                 projectTechnologies={[
-                  "HTML",
-                  "CSS",
-                  "JavaScript",
-                  "Electron.js",
-                  "React",
-                  "Redux",
-                  "lowdb",
+                  'HTML',
+                  'CSS',
+                  'JavaScript',
+                  'Electron.js',
+                  'React',
+                  'Redux',
+                  'lowdb',
                 ]}
                 projectDescription="A weekly budget desktop application that keeps track of your daily purchases throughout the week. Information is saved and stored for up to 12 weeks through the use of an embedded database system."
               />
